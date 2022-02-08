86927535de42223cbe875783f15f1355d707833b
apretie <apramosetie@ualr.edu>
2022-02-08 07:35:17 +0000

    hw 2

--- Assignments/Unit 2 HW/02.08 Merge Two Numbers.py
+++ Assignments/Unit 2 HW/02.08 Merge Two Numbers.py
@@ -1,2 +1,7 @@
 x = int(input("Enter First 2 Digit Number: "))
 y = int(input("Enter Second 2 Digit Number: "))
+onex = (x % 10)
+tenx = (x // 10)%10
+oney = (y % 10)
+teny = (y // 10)%10
+print("Merged Number: {1:1d}{3:1d}{0:1d}{2:1d}".format(onex,tenx,oney,teny))
--- Assignments/Unit 2 HW/02.09 Fractional Part.py
+++ Assignments/Unit 2 HW/02.09 Fractional Part.py
@@ -1,1 +1,5 @@
-x = int(input("Enter a Number: "))
+flt = float(input("Enter a Number: "))
+num = int(flt)
+frac = flt - num
+frac = round(frac,10)
+print("Fractional Part: {}".format(frac))
