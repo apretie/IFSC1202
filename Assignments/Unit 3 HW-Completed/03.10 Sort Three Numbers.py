80e8525e3a0ccef0daf28a363c12dea184528e13
apretie <apramosetie@ualr.edu>
2022-02-09 16:26:01 +0000

    questions about<=

--- Assignments/Unit 3 HW-Completed/03.10 Sort Three Numbers.py
+++ Assignments/Unit 3 HW-Completed/03.10 Sort Three Numbers.py
@@ -1,7 +1,19 @@
 x = int(input("First Number: "))
 y = int(input("Second Number: "))
 z = int(input("Third Number: "))
-asclst = sorted([x,y,z])
 
-if sorted(asclst):
-    print(asclst)
+if x <= y <= z:
+    print(x, y, z)
+elif x <= z <= y:
+    print(x, z, y)
+elif y <= z <= x:
+    print(y, z, x)
+elif y <= x <= z:
+    print(y, x, z)
+elif z <= y <= x:
+    print(z, y, x)
+else:
+    print(z, x, y)
+
+
+# <= can go in the test line not just print
