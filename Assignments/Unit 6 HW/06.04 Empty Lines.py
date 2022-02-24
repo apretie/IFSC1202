2ec3339350b157dfad19243c4c070fdcc2ee205d
apretie <apramosetie@ualr.edu>
2022-02-24 02:58:55 +0000

    test2

--- Assignments/Unit 6 HW/06.04 Empty Lines.py
+++ Assignments/Unit 6 HW/06.04 Empty Lines.py
@@ -1,6 +1,6 @@
 linefile = open("Assignments/Unit 6 HW/Text Files/06.04 EmptyLinesInput.txt", "r")
 linesfile = open("Assignments/Unit 6 HW/Text Files/06.04 EmptyLinesOutput.txt", "w")
 x = linefile.read()
-linesfile = x
 y = linesfile.write()
+linesfile = x
 linesfile = y
