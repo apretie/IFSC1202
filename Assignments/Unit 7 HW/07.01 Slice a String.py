ac7196ff2d343f67a0d41c10e370fc6c24a513e4
apretie <apramosetie@ualr.edu>
2022-03-11 16:18:52 +0000

    hw

--- Assignments/Unit 7 HW/07.01 Slice a String.py
+++ Assignments/Unit 7 HW/07.01 Slice a String.py
@@ -1,16 +1,16 @@
-Prompt for a string:
-#In the first line, print the third character of this string.
-#In the second line, print the second to last character of this string.
-#In the third line, print the first five characters of this string.
-#In the fourth line, print all but the last two characters of this string.
-#In the fifth line, print all the characters of this string with even indices (remember indexing starts at 0, so the characters are displayed starting with the first).
-#In the sixth line, print all the characters of this string with odd indices (i.e. starting with the second character in the string).
-#In the seventh line, print all the characters of the string in reverse order.
-#In the eighth line, print every second character of the string in reverse order, starting from the last one.
-#In the ninth line, print the length of the given string.
+prompt  = str(input("Enter a string: "))
+print(s[3]) #print the third character of this string.
+print(s[2:])#print the second to last character of this string.
+print(s[1:5])#print the first five characters of this string.
+print(s[ :-3]) #print all but the last two characters of this string.
+print(s[3])#print all the characters of this string with even indices (remember indexing starts at 0, so the characters are displayed starting with the first).
+print(s[3])#print all the characters of this string with odd indices (i.e. starting with the second character in the string).
+print(s[3])#print all the characters of the string in reverse order.
+print(s[3])#print every second character of the string in reverse order, starting from the last one.
+print(s[3])#print the length of the given string.
 #Do not use any loops. Each answer can be accomplished in a single line of code.
 # Example: 
-# Enter a string: Hello
+Hello
 # l
 # l
 # Hello
