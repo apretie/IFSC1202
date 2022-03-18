0f9352b05c61dc6b463b63b66668752c8744d09c
apretie <apramosetie@ualr.edu>
2022-03-18 06:25:22 +0000

    .

--- Assignments/Unit 8 HW/08.05 Greater Than Neighbors.py
+++ Assignments/Unit 8 HW/08.05 Greater Than Neighbors.py
@@ -1,8 +1,11 @@
 #Determine and print the quantity of elements that are greater than both of their neighbors.
 #The first and the last items of the list shouldn't be considered because they don't have two neighbors.
-values = input(str("Enter Values Separated by Spaces: "))
+value = input(str("Enter Values Separated by Spaces: "))
 vlist = value.split()
+count = 0
 for i in range(len(vlist)):
     vlist[i] = int(vlist[i])
-
-
+for i in range(1,(len(vlist)-1)):
+    if vlist[i] > vlist[i-1] and vlist[i] > vlist[i+1]:
+        count += 1
+print(count)
