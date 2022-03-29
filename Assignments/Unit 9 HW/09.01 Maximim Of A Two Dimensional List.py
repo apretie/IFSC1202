6d049472b8fdff1c62aa95a67a9798d9457098fc
apretie <apramosetie@ualr.edu>
2022-03-29 02:39:57 +0000

    .

--- Assignments/Unit 9 HW/09.01 Maximim Of A Two Dimensional List.py
+++ Assignments/Unit 9 HW/09.01 Maximim Of A Two Dimensional List.py
@@ -1,11 +1,3 @@
-#Given two integers representing the rows and columns (m×n), and subsequent m rows of n elements
-#Input the number of rows and columns in the list
-#Input the array
-#Print the array.
-#Find the index position of the maximum element and print two numbers representing the index (i×j) or the row number and the column number. 
-#If there exist multiple such elements in different rows, print the one with smaller row number. 
-#If there multiple such elements occur on the same row, output the smallest column number.
-
 a = []
 x = input("Enter the number of rows and columns: ")
 MofN = x.split(" ")
