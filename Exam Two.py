90b3a9dd3a37661c73bbf36521f145e9a350c367
apretie <apramosetie@ualr.edu>
2022-04-04 22:51:55 +0000

    .

--- Exam Two.py
+++ Exam Two.py
@@ -1,21 +1,37 @@
+namefile = open("/workspace/IFSC1202/Exam Two Names.txt","r")
+nametxt = namefile.readline()
 a =[]
 rank = 0
-openfile = open("/workspace/IFSC1202/Exam Two Names.txt","r")
-nametxt = openfile.readlines()
-def ProperCase(s):
-    s = s.lower()
-    return s[0:1].upper() + s[1:].lower()
+found = "no"
+while nametxt != "":
+    names = nametxt.split(",")
+    for i in range(len(names)):
+        names[i] = str(names[i]).strip(" ")
+    a.append(names)
+    nametxt = namefile.readline()
+namefile.close()
+#print(a[0][0]) 
+fnandr = str(input("Enter a Name: ")) #find name and rank
+fnandr = fnandr.strip(" ")
+fnandr = fnandr.title()
+if fnandr == "Q":
+    exit()
+j = 5
+for i in range(len(a)):
+    rank += 1
+    for j in range(len(a[i])):
+        #print(a[i][j])
+        #print("fnandr is {}".format(fnandr))
+       if fnandr == a[i][j]:
+            if j == 0:
+                print("Boy's Name - Rank: {}".format(rank))
+            elif j == 1:
+                print("Girl's Name - Rank: {}".format(rank))
+            #found = "yes"
+            #print("Boy's Name - Rank: {}".format(rank))
+if j == 5:
+    print("Name Not Found")
+        #fnandr != a[i][j]
 
-something = file.readline()
-while something != "":
-    something.split(",")
-    something.strip(" ")
-    girlname =[:]
-    boyname = [:]
-    
-    for i in range((len(girlname)):
-        rank = girlname + 1
-        girlname.append()
-    for i in range(len(boyname)):
-        boyname.append()
-fnandr = input("Enter a Name: ") #find name and rank
+            
+#
