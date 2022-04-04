e408e4290f82e761dd58bc150ca9a5337e992ee2
apretie <apramosetie@ualr.edu>
2022-04-04 21:19:42 +0000

    .

---
+++ Exam Two.py
@@ -0,0 +1,21 @@
+a =[]
+rank = 0
+openfile = open("/workspace/IFSC1202/Exam Two Names.txt","r")
+nametxt = openfile.readlines()
+def ProperCase(s):
+    s = s.lower()
+    return s[0:1].upper() + s[1:].lower()
+
+something = file.readline()
+while something != "":
+    something.split(",")
+    something.strip(" ")
+    girlname =[:]
+    boyname = [:]
+    
+    for i in range(len(girlname)):
+        
+        girlname.append()
+    for i in range(len(boyname)):
+        boyname.append()
+fnandr = input("Enter a Name: ") #find name and rank
